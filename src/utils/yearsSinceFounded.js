11aa1ea25501d82bd3b78129bcf27e8d1d26f0d5
DavidOrtego <davidortego2016@gmail.com>
2026-02-25 18:54:24 +0100

    feat: add yearsSinceFounded utility function to calculate years since founding

---
+++ src/utils/yearsSinceFounded.js
@@ -0,0 +1,9 @@
+/**
+ * Calcular los años desde el año de fundación hasta el año actual.
+ * @param {number} foundedYear - Año de fundación.
+ * @param {number} [currentYear] - Año actual.
+ * @returns {number} Años desde el año de fundación hasta el año actual.
+ */
+function yearsSinceFounded(foundedYear, currentYear = new Date().getFullYear()) {
+  return currentYear - foundedYear;
+}
